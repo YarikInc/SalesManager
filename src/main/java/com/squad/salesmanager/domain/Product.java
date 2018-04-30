06b68340d1349222688ca3da756234c244c50106
ruslan2203 <ruslan0505@gmail.com>
2018-04-30 23:38:03 +0300

    Revert "Create Domain Objects"
    This reverts commit 8d0118c

--- src/main/java/com/squad/salesmanager/domain/Product.java
+++ src/main/java/com/squad/salesmanager/domain/Product.java
@@ -2,48 +2,5 @@ package com.squad.salesmanager.domain;
 
 public class Product {
 
-    ProductCategory productCategory;
-    String nameProduct;
-    double productPacking;
-    double price;
 
-    public ProductCategory getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(ProductCategory productCategory) {
-        this.productCategory = productCategory;
-    }
-
-    public String getNameProduct() {
-        return nameProduct;
-    }
-
-    public void setNameProduct(String nameProduct) {
-        this.nameProduct = nameProduct;
-    }
-
-    public double getProductPacking() {
-        return productPacking;
-    }
-
-    public void setProductPacking(double productPacking) {
-        this.productPacking = productPacking;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Product(ProductCategory productCategory, String nameProduct, double productPacking, double price) {
-        this.productCategory = productCategory;
-        this.nameProduct = nameProduct;
-        this.productPacking = productPacking;
-        this.price = price;
-    }
 }
-
--- src/main/java/com/squad/salesmanager/domain/ProductCategory.java
+++ src/main/java/com/squad/salesmanager/domain/ProductCategory.java
@@ -1,6 +1,4 @@
 package com.squad.salesmanager.domain;
 
 public enum ProductCategory {
-
-    PackedSweets, WeightCandy, Biscuit, Shtuchka, PackagedSweets, coffe
 }
