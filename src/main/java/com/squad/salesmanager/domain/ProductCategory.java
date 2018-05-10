e9bfa0ef8065f482292f7bf348bbe57c7263079f
ruslan2203 <ruslan0505@gmail.com>
2018-05-10 21:39:59 +0300

    Making changes in uppercase letters

--- src/main/java/com/squad/salesmanager/domain/ProductCategory.java
+++ src/main/java/com/squad/salesmanager/domain/ProductCategory.java
@@ -1,6 +1,6 @@
 package com.squad.salesmanager.domain;
 
 public enum ProductCategory {
-    PackedSweets, WeightCandy, Biscuit, Shtuchka, PackagedSweets, coffe
+    PACKEDSWETS, WEIGHTCANDY, BISCUIT, SHTUCHKA, PACKAGEDSWEETS, COFFE
 }
 
